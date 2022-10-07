5ebfbc25f3825b6b5413d54355b8d8a1eff58142
3etimds <3etimds@LAB2-09>
2022-10-07 11:52:54 -0300

    Criação das Models de Produto e Souvenir
    ainda a adicionar as inserções dos métodos para inserção (ligar produto e Livro e produto e souvenir para inserir tudo de uma vez)

---
+++ API_SpringLibrary/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_SpringLibrary.Models
+{
+    public class Produto
+    {
+        public int IdProd { get; set; } 
+        public string NomeProd { get; set; }    
+        public int QtdProd { get; set; }
+        public float Preco { get; set; }    
+
+
+    }
+}
