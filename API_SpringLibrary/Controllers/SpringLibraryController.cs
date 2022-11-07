0c79c3fc7f486b70f80070fdff463015b95ecb03
letyresina <leticiaresina@gmail.com>
2022-11-07 10:12:06 -0300

    Iniciando a programação da Controller principal
    Fazer ajustes pois mudou o banco!! Está nos comentários!!

--- API_SpringLibrary/Controllers/SpringLibraryController.cs
+++ API_SpringLibrary/Controllers/SpringLibraryController.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using API_SpringLibrary.Models;
+using MySql.Data.MySqlClient;
+
 
 namespace API_SpringLibrary.Controllers
 {
     public class SpringLibraryController : ApiController
     {
-      //Porgramar
+        // Para encontrar db
+        DatabaseHelper db = new DatabaseHelper();
+
+        // Models 
+
+        Autor aut = new Autor();
+        Cliente cli = new Cliente();
+        ClienteFisico cliFis = new ClienteFisico();
+        ClienteJuridico cliJur = new ClienteJuridico();
+        Editora edit = new Editora();
+        //Tirou a tabela endereço, consertar na API 
+        Genero gen = new Genero();
+        Livro liv = new Livro();
+        //Tirou produto também, então consertar a API
     }
 }
