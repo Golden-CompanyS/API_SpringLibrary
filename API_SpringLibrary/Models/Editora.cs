e80add95bce4fcb48e7e4d1fa7be807acd9eeeea
letyresina <leticiaresina@gmail.com>
2022-11-15 16:18:55 -0300

    Finalizado a programação na Model Editora. Ainda não testado

--- API_SpringLibrary/Models/Editora.cs
+++ API_SpringLibrary/Models/Editora.cs
@@ -35,29 +35,71 @@ namespace API_SpringLibrary.Models
             Editora tempEdit = new Editora();
             if (reader.Read())
             {
-                tempEdit.IdEdit = int.Parse(reader["IdEdit"].ToString());
-                tempEdit.NomEdit = reader["NomEdit"].ToString();
-                tempEdit.CelEdit = reader["CelEdit"].ToString();
-                tempEdit.EmailEdit = reader["EmailEdit"].ToString();
+                tempEdit.IdEdit = int.Parse(reader["idEdit"].ToString());
+                tempEdit.NomEdit = reader["nomEdit"].ToString();
+                tempEdit.CelEdit = reader["celEdit"].ToString();
+                tempEdit.EmailEdit = reader["emailEdit"].ToString();
             }
             return tempEdit;
         }
-        public List<Editora> AssignArts(MySqlDataReader reader)
+        public List<Editora> AssignEdits(MySqlDataReader reader)
         {
             List<Editora> editList = new List<Editora>();
             while (reader.Read())
             {
                 Editora tempEdit = new Editora();
-                tempEdit.IdEdit = int.Parse(reader["IdEdit"].ToString());
-                tempEdit.NomEdit = reader["NomEdit"].ToString();
-                tempEdit.CelEdit = reader["CelEdit"].ToString();
-                tempEdit.EmailEdit = reader["EmailEdit"].ToString();
-                artList.Add(tempEdit);
+                tempEdit.IdEdit = int.Parse(reader["idEdit"].ToString());
+                tempEdit.NomEdit = reader["nomEdit"].ToString();
+                tempEdit.CelEdit = reader["celEdit"].ToString();
+                tempEdit.EmailEdit = reader["emailEdit"].ToString();
+                editList.Add(tempEdit);
             }
             return editList;
         }
 
-
-
+        //comandos a partir daqui
+        public List<Editora> GetAllArts()
+        {
+            command.CommandText = ("call spcheckAllEdit()");
+            var reader = command.ExecuteReader();
+            List<Editora> edits = this.AssignEdits(reader);
+            return edits;
+        }
+        public Editora GetEditById(int id)
+        {
+            command.CommandText = ("call spcheckEditById(id);");
+            command.Parameters.Add("@id", MySqlDbType.Int64).Value = id;
+            var reader = command.ExecuteReader();
+            Editora res = this.AssignEdit(reader);
+            return res;
+        }
+        public void PostNewEditora(Editora edit)
+        {
+            string query =
+                "call spcadEdit('nome', 'celular', 'email')";
+            query = query.Replace("nome", edit.NomEdit);
+            query = query.Replace("celular", edit.CelEdit);
+            query = query.Replace("email", edit.EmailEdit);
+            command.CommandText = query;
+            var executor = command.ExecuteNonQuery();
+        }
+        public void AlterEdit(int id, Editora edit)
+        {
+            string query =
+                "call spaltEdit(id, 'nome', 'celular', 'email')";
+            query = query.Replace("id", id.ToString());
+            query = query.Replace("nome", edit.NomEdit);
+            query = query.Replace("celular", edit.CelEdit);
+            query = query.Replace("email", edit.EmailEdit);
+            command.CommandText = query;
+            var executor = command.ExecuteNonQuery();
+        }
+        public void DeleteEdit(int id)
+        {
+            string query = "delete from tbEditora where idEdit = id;";
+            query = query.Replace("id", id.ToString());
+            command.CommandText = query;
+            var executor = command.ExecuteNonQuery();
+        }
     }
 }
