48b91c7db735360962e919192fa4f4e2a2804899
letyresina <leticiaresina@gmail.com>
2022-11-19 20:56:19 -0300

    Iniciando a programação da classe Cliente Jurídico

--- API_SpringLibrary/Models/ClienteJuridico.cs
+++ API_SpringLibrary/Models/ClienteJuridico.cs
@@ -9,5 +9,31 @@ namespace API_SpringLibrary.Models
 {
     public class ClienteJuridico
     {
+        public ClienteJuridico(string cnpjCli, int idCli, string fantaCliJ, string represCliJ)
+        {
+            CNPJCli = cnpjCli;
+            IdCli = idCli;
+            FantaCliJ = fantaCliJ;
+            RepresCliJ = represCliJ;
+        }
+
+        public ClienteJuridico(string cnpjCli, string fantaCliJ, string represCliJ)
+        {
+            CNPJCli = cnpjCli;
+            FantaCliJ = fantaCliJ;
+            RepresCliJ = represCliJ;
+        }
+
+        public ClienteJuridico()
+        {
+        }
+
+        public string CNPJCli { get; set; }
+
+        public int IdCli { get; set; }
+
+        public string FantaCliJ { get; set; }
+
+        public string RepresCliJ { get; set; }
     }
 }
