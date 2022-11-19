60092dd9f2923e84ace3e80172ea68c979382ee4
letyresina <leticiaresina@gmail.com>
2022-11-19 19:56:56 -0300

    Iniciando a programação do Cliente Físico

--- API_SpringLibrary/Models/ClienteFisico.cs
+++ API_SpringLibrary/Models/ClienteFisico.cs
@@ -4,13 +4,34 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using API_SpringLibrary.Models;
 
 namespace API_SpringLibrary.Models
 {
     public class ClienteFisico
     {
-        //Finalizar
-      
+        public ClienteFisico(string cpfCli, int idCli, DateTime dtNascCliF)
+        {
+            CPFCliF = cpfCli;
+            IdCli = idCli;
+            DtNascCliF = dtNascCliF;
+        }
+
+        public ClienteFisico(string cpfCli, DateTime dtNascCliF)
+        {
+            CPFCliF = cpfCli;
+            DtNascCliF = dtNascCliF;
+        }
+
+        public ClienteFisico()
+        {
+        }
+
+       public string CPFCliF { get; set; }
+
+       public int IdCli { get; set; }
+
+        public DateTime DtNascCliF { get; set; }
 
        //Metódos 
     }
