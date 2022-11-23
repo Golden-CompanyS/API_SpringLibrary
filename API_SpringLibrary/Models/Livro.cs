9f2e834ac75de3f4e4ab3d5bcb029a4c19093d9c
letyresina <leticiaresina@gmail.com>
2022-11-23 08:37:37 -0300

    Iniciando a programação da classe llivro
    Finalizar.

--- API_SpringLibrary/Models/Livro.cs
+++ API_SpringLibrary/Models/Livro.cs
@@ -10,5 +10,35 @@ namespace API_SpringLibrary.Models
 {
     public class Livro
     {
+        /* anoLiv SMALLINT NOT NULL,
+        precoLiv FLOAT(10,2) NOT NULL,
+        qtdLiv INT DEFAULT(0),
+        ativoLiv boolean,
+        idEdit INT NOT NULL,
+        FOREIGN KEY(idEdit)
+            REFERENCES tbEditora(idEdit),
+        idGen INT NOT NULL,
+        FOREIGN KEY(idGen)
+            REFERENCES tbGenero(idGen),
+        idFunc INT NOT NULL,
+        FOREIGN KEY(idFunc) 
+         REFERENCES tbFuncionario(idFunc) */
+
+
+        public string ISBNLiv { get; set; }
+
+        public string TitLiv { get; set; }
+
+        public string TitOriLiv { get; set; }
+
+        public string SinopLiv { get; set; }
+
+        public string ImgLiv { get; set; }
+
+        public string PratLiv { get; set; }
+
+        public int NumPagLiv { get; set; }
+
+        public int NumEdicaoLiv { get; set; }
     }
 }
