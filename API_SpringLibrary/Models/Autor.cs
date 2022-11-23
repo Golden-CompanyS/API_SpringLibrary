0bd70e4b9f91cb9069e40896082ecbb5c8134a07
letyresina <leticiaresina@gmail.com>
2022-11-23 07:35:03 -0300

    Finalizando a classe autor
    Não testada.

--- API_SpringLibrary/Models/Autor.cs
+++ API_SpringLibrary/Models/Autor.cs
@@ -21,6 +21,10 @@ namespace API_SpringLibrary.Models
             NomAut = nomAut;
         }
 
+        public Autor()
+        {
+        }
+
         public int IdAut { get; set; }
 
         public string NomAut { get; set; }
@@ -28,6 +32,78 @@ namespace API_SpringLibrary.Models
         MySqlCommand command = DatabaseHelper.CriaComando();
 
         // Agiliza o reader pra não ter que ficar repetindo com comando abaixo!
-        
+        public Autor AssignAut(MySqlDataReader reader)
+        {
+            Autor tempAut = new Autor();
+            if (reader.Read())
+            {
+                tempAut.IdAut = int.Parse(reader["idAut"].ToString());
+                tempAut.NomAut = reader["nomAut"].ToString();
+            }
+            return tempAut;
         }
+
+        public List<Autor> AssignAuts(MySqlDataReader reader)
+        {
+            List<Autor> editList = new List<Autor>();
+            while (reader.Read())
+            {
+                Autor tempAut = new Autor();
+                tempAut.IdAut = int.Parse(reader["idAut"].ToString());
+                tempAut.NomAut = reader["nomAut"].ToString();
+                editList.Add(tempAut);
+            }
+            return editList;
+        }
+
+        //Metódos
+
+        public List<Autor> GetAllAutores()
+        {
+            command.CommandText = ("call spcheckAllAut();");
+            var reader = command.ExecuteReader();
+            List<Autor> autores = this.AssignAuts(reader);
+            return autores;
+        }
+
+        public Autor GetAutById(int id)
+        {
+            command.CommandText = ("call spcheckAutById(@id);");
+            command.Parameters.Add("@id", MySqlDbType.Int64).Value = id;
+            var reader = command.ExecuteReader();
+            Autor res = this.AssignAut(reader);
+            return res;
+        }
+
+        public IEnumerable<Autor> GetAutByParameter(string column, string parVal)
+        {
+            string query = "select * from tbAutor where placeholder like 'parameter%'";
+            query = query.Replace("parameter", parVal);
+            query = query.Replace("placeholder", column);
+            command.CommandText = query;
+            var reader = command.ExecuteReader();
+            IEnumerable<Autor> res = this.AssignAuts(reader);
+            return res;
+        }
+
+        public void PostNewAutor(Autor aut)
+        {
+            string query =
+                "call spcadAut('nome');";
+            query = query.Replace("nome", aut.NomAut);
+            command.CommandText = query;
+            var executor = command.ExecuteNonQuery();
+        }
+
+        public void AlterAutor(int id, Autor aut)
+        {
+            string query =
+                "call spaltAut(id, 'nome');";
+            query = query.Replace("id", id.ToString());
+            query = query.Replace("nome", aut.NomAut);
+            command.CommandText = query;
+            var executor = command.ExecuteNonQuery();
+        }
+
+    }
     }
