fc7a565bb4d5f151360b342381a9d44c7593ecc9
letyresina <leticiaresina@gmail.com>
2022-11-22 22:35:49 -0300

    Iniciando a programação de Autor

--- API_SpringLibrary/Models/Autor.cs
+++ API_SpringLibrary/Models/Autor.cs
@@ -10,6 +10,24 @@ namespace API_SpringLibrary.Models
 {
     public class Autor
     {
-       
+        public Autor(int idAut, string nomAut)
+        {
+            IdAut = idAut;
+            NomAut = nomAut;
+        }
+
+        public Autor(string nomAut)
+        {
+            NomAut = nomAut;
+        }
+
+        public int IdAut { get; set; }
+
+        public string NomAut { get; set; }
+
+        MySqlCommand command = DatabaseHelper.CriaComando();
+
+        // Agiliza o reader pra não ter que ficar repetindo com comando abaixo!
+        
+        }
     }
-}
