66102aa5a232555443670472c73e1774ec025c93
letyresina <leticiaresina@gmail.com>
2022-11-24 16:34:08 -0300

    Criação da classe LivroAutor que faltava

---
+++ API_SpringLibrary/Models/LivroAutor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_SpringLibrary.Models
+{
+    public class LivroAutor
+    {
+        public string ISBNLiv { get; set; }
+
+        public int IdAut { get; set; }
+    }
+}
